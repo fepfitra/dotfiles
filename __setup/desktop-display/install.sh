94be75ce4606adf850b98b998e1f72a6eb866876
fepfitra <fitriansyahekaputra@mail.ugm.ac.id>
2024-03-08 09:38:47 +0700

    fix move theme permission

--- __setup/desktop-display/install.sh
+++ __setup/desktop-display/install.sh
@@ -2,7 +2,7 @@ yay -Syu qt5-graphicaleffects qt5-svg qt5-quickcontrols2 --noconfirm
 yay -S sddm i3 powerline bumblebee-status --noconfirm
 
 git clone https://github.com/catppuccin/sddm.git
-mv ./sddm/src/* /usr/share/sddm/themes/
+sudo mv ./sddm/src/* /usr/share/sddm/themes/
 rm -rf sddm
 sudo mv ./default.conf /usr/lib/sddm/sddm.conf.d
 
