e3992b6a73a2d44241adc84123632e7f084db657
fepfitra <fitriansyahekaputra@mail.ugm.ac.id>
2024-03-08 09:40:23 +0700

    idk why conf missing

--- __setup/desktop-display/install.sh
+++ __setup/desktop-display/install.sh
@@ -3,6 +3,6 @@ yay -S sddm i3 powerline bumblebee-status --noconfirm
 
 git clone https://github.com/catppuccin/sddm.git
 sudo mv ./sddm/src/* /usr/share/sddm/themes/
-rm -rf sddm
 sudo mv ./default.conf /usr/lib/sddm/sddm.conf.d
+rm -rf ./sddm
 
