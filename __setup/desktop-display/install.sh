94a13b0b83d4e1820cfc412247fdcbdebd7b5256
fepfitra <fitriansyahekaputra@mail.ugm.ac.id>
2024-03-08 09:44:45 +0700

    force overwrite

--- __setup/desktop-display/install.sh
+++ __setup/desktop-display/install.sh
@@ -2,7 +2,8 @@ yay -Syu qt5-graphicaleffects qt5-svg qt5-quickcontrols2 --noconfirm
 yay -S sddm i3 powerline bumblebee-status --noconfirm
 
 git clone https://github.com/catppuccin/sddm.git
-sudo mv ./sddm/src/* /usr/share/sddm/themes/
-sudo mv ./default.conf /usr/lib/sddm/sddm.conf.d
+sudo rm -rf /usr/share/sddm/themes/*
+sudo mv -f ./sddm/src/* /usr/share/sddm/themes/
+sudo mv -f ./default.conf /usr/lib/sddm/sddm.conf.d
 rm -rf ./sddm
 
