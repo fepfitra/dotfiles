d336116c8e5a25f9bc17c39eb270c554e85cc0a1
fepfitra <fitriansyahekaputra@mail.ugm.ac.id>
2024-03-08 09:36:54 +0700

    fix clone git

--- __setup/desktop-display/install.sh
+++ __setup/desktop-display/install.sh
@@ -1,7 +1,7 @@
 yay -Syu qt5-graphicaleffects qt5-svg qt5-quickcontrols2 --noconfirm
 yay -S sddm i3 powerline bumblebee-status --noconfirm
 
-git clone git@github.com:catppuccin/sddm.git
+git clone https://github.com/catppuccin/sddm.git
 mv ./sddm/src/* /usr/share/sddm/themes/
 rm -rf sddm
 sudo mv ./default.conf /usr/lib/sddm/sddm.conf.d
