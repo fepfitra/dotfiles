0182a1cb909bcfb40af52cd5a494f0ce0ff9b48c
fepfitra <fitriansyahekaputra@mail.ugm.ac.id>
2024-03-09 19:53:14 +0700

    remove unnecessary comment

--- setup.sh
+++ setup.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 
-#wource
 #https://dev.to/spacerockmedia/how-i-manage-my-dotfiles-using-gnu-stow-4l59
 
 # make sure we have pulled in and updated any submodules
 git submodule init
 git submodule update
 
-# what directories should be installable by all users including the root user
 base=(
 )
 
-# folders that should, or only need to be installed for a local user
 useronly=(
   git
   fish
@@ -23,7 +20,6 @@ useronly=(
   fonts
 )
 
-# run the stow command for the passed in directory ($2) in location $1
 stowit() {
   usr=$1
   app=$2
@@ -36,12 +32,10 @@ stowit() {
 echo ""
 echo "Stowing apps for user: ${whoami}"
 
-#install apps available to local users and root
 for app in ${base[@]}; do
   stowit "${HOME}" $app 
 done
 
-# install only user space folders
 for app in ${useronly[@]}; do
   if [[ ! "$(whoami)" = *"root"* ]]; then
     stowit "${HOME}" $app 
