db6a166b6c597481b8ac0d0dd11725119beb8163
fepfitra <fitriansyahekaputra@mail.ugm.ac.id>
2024-03-14 04:16:48 +0700

    add toggle keybind

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -3,5 +3,6 @@ return {
   dependencies = 'nvim-tree/nvim-web-devicons',
   config = function()
     require("nvim-tree").setup()
+    vim.keymap.set('n', '<leader>e', ':NvimTreeToggle\n', {})
   end
 }
