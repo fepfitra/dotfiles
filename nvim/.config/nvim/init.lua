83b8a4647dc054b90efd0a27c3c1ffd446811db3
fepfitra <fitriansyahekaputra@mail.ugm.ac.id>
2024-03-14 22:32:29 +0700

    nvim add initial setup

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,6 +6,9 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("set cursorline")
+vim.cmd("set undofile")
 vim.cmd("set clipboard=unnamedplus")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
