908b2fb4e10fac42f68e49ef0ff70fa0df1acccd
fepfitra <fitriansyahekaputra@mail.ugm.ac.id>
2024-03-14 20:51:58 +0700

    add more lsp

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -20,13 +20,46 @@ return {
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
       local lspconfig = require("lspconfig")
-      lspconfig.tsserver.setup({
+      lspconfig.bashls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.clangd.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.gopls.setup({
         capabilities = capabilities,
       })
       lspconfig.html.setup({
         capabilities = capabilities,
       })
+      lspconfig.intelephense.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.jsonls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.kotlin_language_server.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.lemminx.setup({
+        capabilities = capabilities,
+      })
       lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.pyright.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.rust_analyzer.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.tailwindcss.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.tsserver.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.yamlls.setup({
         capabilities = capabilities,
       })
 
