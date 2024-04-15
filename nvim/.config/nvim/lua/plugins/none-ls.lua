fc672922fe25dbb867e8f38d4ef1f1dbb53353eb
fepfitra <fitriansyahekaputra@mail.ugm.ac.id>
2024-04-15 10:43:29 +0700

    nvim add latex lsp

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -7,7 +7,6 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
-
         require("none-ls.diagnostics.eslint_d"),
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
@@ -15,6 +14,8 @@ return {
 				-- null_ls.builtins.diagnostics.eslint_d,
 				null_ls.builtins.diagnostics.rubocop,
 				null_ls.builtins.formatting.rubocop,
+				-- null_ls.builtins.formatting.bibtex_tidy,
+				-- null_ls.builtins.formatting.latexindent,
 			},
 		})
 
