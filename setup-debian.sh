680f079f8f27ab903da793920579d54fcb938eeb
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2024-07-06 19:54:09 +0700

    add setup for debian

---
+++ setup-debian.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+#https://dev.to/spacerockmedia/how-i-manage-my-dotfiles-using-gnu-stow-4l59
+
+# make sure we have pulled in and updated any submodules
+git submodule init
+git submodule update
+
+base=(
+  fish
+  nvim
+  tmux
+)
+
+stowit() {
+  usr=$1
+  app=$2
+  # -v verbose
+  # -R recursive
+  # -t target
+  stow -v -R -t ${usr} ${app}
+}
+
+echo ""
+echo "Stowing apps for user: ${whoami}"
+
+for app in ${base[@]}; do
+  stowit "${HOME}" $app 
+done
+
+$(pwd)/apps.sh
+$(pwd)/modules.sh
+
+echo ""
+echo "##### ALL DONE"
