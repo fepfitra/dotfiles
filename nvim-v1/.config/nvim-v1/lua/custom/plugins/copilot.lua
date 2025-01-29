ba5c8deff1d412cc1d8a9e713d35593fae007fdd
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2025-01-29 23:51:09 +0700

    feat: add copilot

--- nvim-v1/.config/nvim-v1/lua/custom/plugins/copilot.lua
+++ nvim-v1/.config/nvim-v1/lua/custom/plugins/copilot.lua
@@ -1,3 +1,13 @@
 return {
+  {
   'github/copilot.vim',
+  config = function()
+  end,
+  },
+  -- {
+  --   "zbirenbaum/copilot-cmp",
+  --   config = function ()
+  --     require("copilot_cmp").setup()
+  --   end
+  -- }
 }
