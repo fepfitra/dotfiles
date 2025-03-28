2fdd2fcae0bc284f5476d605165bc0e5f7ed5091
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2025-03-28 20:05:16 +0700

    blink cmp

--- nvim-v1/.config/nvim-v1/lua/custom/plugins/blink.lua
+++ nvim-v1/.config/nvim-v1/lua/custom/plugins/blink.lua
@@ -1,39 +1,90 @@
 return {
-  'saghen/blink.cmp',
-  -- optional: provides snippets for the snippet source
-  dependencies = 'rafamadriz/friendly-snippets',
-
-  -- use a release tag to download pre-built binaries
-  version = '*',
-  -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-  -- build = 'cargo build --release',
-  -- If you use nix, you can build from source using latest nightly rust with:
-  -- build = 'nix run .#build-plugin',
-
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
-  opts = {
-    -- 'default' for mappings similar to built-in completion
-    -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-    -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-    -- See the full "keymap" documentation for information on defining your own keymap.
-    keymap = { preset = 'default' },
-
-    appearance = {
-      -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-      -- Useful for when your theme doesn't support blink.cmp
-      -- Will be removed in a future release
-      use_nvim_cmp_as_default = true,
-      -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- Adjusts spacing to ensure icons are aligned
-      nerd_font_variant = 'mono',
+  'saghen/blink.nvim',
+  build = 'cargo build --release', -- for delimiters
+  keys = {
+    -- chartoggle
+    {
+      ';',
+      function()
+        require('blink.chartoggle').toggle_char_eol ';'
+      end,
+      mode = { 'n', 'v' },
+      desc = 'Toggle ; at eol',
+    },
+    {
+      ',',
+      function()
+        require('blink.chartoggle').toggle_char_eol ','
+      end,
+      mode = { 'n', 'v' },
+      desc = 'Toggle , at eol',
     },
 
-    -- Default list of enabled providers defined so that you can extend it
-    -- elsewhere in your config, without redefining it, due to `opts_extend`
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
+    -- tree
+    -- { '<C-e>', '<cmd>BlinkTree reveal<cr>', desc = 'Reveal current file in tree' },
+    -- { '<leader>E', '<cmd>BlinkTree toggle<cr>', desc = 'Reveal current file in tree' },
+    -- { '<leader>e', '<cmd>BlinkTree toggle-focus<cr>', desc = 'Toggle file tree focus' },
+  },
+  -- all modules handle lazy loading internally
+  lazy = false,
+  opts = {
+    chartoggle = { enabled = true },
+    delimiters = {
+      enabled = true,
+      highlights = {
+        'RainbowOrange',
+        'RainbowPurple',
+        'RainbowBlue',
+      },
+      priority = 200,
+      ns = vim.api.nvim_create_namespace 'blink.delimiters',
+      debug = false,
+    },
+    indent = {
+      enabled = true,
+      -- start with indent guides visible
+      visible = true,
+      blocked = {
+        buftypes = {},
+        filetypes = {},
+      },
+      static = {
+        enabled = true,
+        char = '▎',
+        priority = 1,
+        -- specify multiple highlights here for rainbow-style indent guides
+        -- highlights = { 'BlinkIndentRed', 'BlinkIndentOrange', 'BlinkIndentYellow', 'BlinkIndentGreen', 'BlinkIndentViolet', 'BlinkIndentCyan' },
+        highlights = { 'BlinkIndent' },
+      },
+      scope = {
+        enabled = true,
+        char = '▎',
+        priority = 1024,
+        -- set this to a single highlight, such as 'BlinkIndent' to disable rainbow-style indent guides
+        -- highlights = { 'BlinkIndent' },
+        highlights = {
+          'BlinkIndentRed',
+          'BlinkIndentYellow',
+          'BlinkIndentBlue',
+          'BlinkIndentOrange',
+          'BlinkIndentGreen',
+          'BlinkIndentViolet',
+          'BlinkIndentCyan',
+        },
+        underline = {
+          -- enable to show underlines on the line above the current scope
+          enabled = false,
+          highlights = {
+            'BlinkIndentRedUnderline',
+            'BlinkIndentYellowUnderline',
+            'BlinkIndentBlueUnderline',
+            'BlinkIndentOrangeUnderline',
+            'BlinkIndentGreenUnderline',
+            'BlinkIndentVioletUnderline',
+            'BlinkIndentCyanUnderline',
+          },
+        },
+      },
     },
   },
-  opts_extend = { 'sources.default' },
 }
