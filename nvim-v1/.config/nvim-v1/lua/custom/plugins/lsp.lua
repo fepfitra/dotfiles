54cf9507d35ecfa05ffd94a751a1d2b3861c7519
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2025-07-05 16:54:15 +0700

    feat: add astro

--- nvim-v1/.config/nvim-v1/lua/custom/plugins/lsp.lua
+++ nvim-v1/.config/nvim-v1/lua/custom/plugins/lsp.lua
@@ -191,6 +191,11 @@ return {
         -- ts_ls = {},
         --
 
+        astro = {
+          capabilities = capabilities,
+          on_attach = on_attach,
+          filetypes = { 'astro' },
+        },
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
@@ -223,6 +228,7 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'astro',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
