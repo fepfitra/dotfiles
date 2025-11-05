c7c149dc7a2b91d00c7e036404a287bfcd3fd22c
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2025-11-05 21:41:02 +0700

    nvim: change dianostic icon

--- nvim-v1/.config/nvim-v1/lua/custom/plugins/lsp.lua
+++ nvim-v1/.config/nvim-v1/lua/custom/plugins/lsp.lua
@@ -152,14 +152,14 @@ return {
       })
 
       -- Change diagnostic symbols in the sign column (gutter)
-      -- if vim.g.have_nerd_font then
-      --   local signs = { ERROR = '', WARN = '', INFO = '', HINT = '' }
-      --   local diagnostic_signs = {}
-      --   for type, icon in pairs(signs) do
-      --     diagnostic_signs[vim.diagnostic.severity[type]] = icon
-      --   end
-      --   vim.diagnostic.config { signs = { text = diagnostic_signs } }
-      -- end
+      if vim.g.have_nerd_font then
+        local signs = { ERROR = '', WARN = '', INFO = '', HINT = '' }
+        local diagnostic_signs = {}
+        for type, icon in pairs(signs) do
+          diagnostic_signs[vim.diagnostic.severity[type]] = icon
+        end
+        vim.diagnostic.config { signs = { text = diagnostic_signs } }
+      end
 
       -- LSP servers and clients are able to communicate to each other what features they support.
       --  By default, Neovim doesn't support everything that is in the LSP specification.
@@ -193,7 +193,7 @@ return {
 
         astro = {
           capabilities = capabilities,
-          on_attach = on_attach,
+          -- on_attach = on_attach,
           filetypes = { 'astro' },
         },
         lua_ls = {
@@ -243,6 +243,8 @@ return {
             require('lspconfig')[server_name].setup(server)
           end,
         },
+        ensure_installed = ensure_installed,
+        automatic_enable = true,
       }
     end,
   },
