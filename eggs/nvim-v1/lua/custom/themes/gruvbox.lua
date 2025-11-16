fd33f14ff8c463b36821062c8587c04a620306b4
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2025-11-16 20:39:39 +0700

    nvim: change theme repo

--- eggs/nvim-v1/lua/custom/themes/gruvbox.lua
+++ eggs/nvim-v1/lua/custom/themes/gruvbox.lua
@@ -1,4 +1,10 @@
 return {
-  'morhetz/gruvbox',
+  'ellisonleao/gruvbox.nvim',
   priority = 1000,
+  config = true,
+  opts = {
+    overrides = {
+      LspInlayHint = { fg = '#d3869b', italic = true, bg = 'NONE' },
+    },
+  },
 }
