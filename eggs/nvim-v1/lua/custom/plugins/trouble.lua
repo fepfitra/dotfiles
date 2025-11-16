5e6343a82c2f9292861ba56b7eb7df40390fe8de
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2025-11-16 20:41:35 +0700

    nvim: add more toggle

--- eggs/nvim-v1/lua/custom/plugins/trouble.lua
+++ eggs/nvim-v1/lua/custom/plugins/trouble.lua
@@ -1,37 +1,30 @@
 return {
   'folke/trouble.nvim',
-  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  opts = {
+    modes = {
+      symbols = {
+        win = {
+          position = 'left',
+        },
+      },
+    },
+  },
   cmd = 'Trouble',
   keys = {
     {
-      '<leader>xx',
-      '<cmd>Trouble diagnostics toggle<cr>',
-      desc = 'Diagnostics (Trouble)',
-    },
-    {
-      '<leader>xX',
-      '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
-      desc = 'Buffer Diagnostics (Trouble)',
-    },
-    {
-      '<leader>cs',
-      '<cmd>Trouble symbols toggle focus=false<cr>',
+      '<leader>ts',
+      '<cmd>Trouble symbols toggle focus=true<cr>',
       desc = 'Symbols (Trouble)',
     },
     {
-      '<leader>cl',
-      '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
-      desc = 'LSP Definitions / references / ... (Trouble)',
+      '<leader>tt',
+      '<cmd>Trouble todo toggle focus=true<cr>',
+      desc = 'Todo (Trouble)',
     },
     {
-      '<leader>xL',
-      '<cmd>Trouble loclist toggle<cr>',
-      desc = 'Location List (Trouble)',
-    },
-    {
-      '<leader>xQ',
-      '<cmd>Trouble qflist toggle<cr>',
-      desc = 'Quickfix List (Trouble)',
+      '<leader>td',
+      '<cmd>Trouble diagnostics toggle focus=true<cr>',
+      desc = 'Diagnostics (Trouble)',
     },
   },
 }
