fcc5d0f61f104ee7c51b5f95c0c7f588130e756c
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2025-12-22 15:36:39 +0700

    nvim: add vscode-diff.nvim

---
+++ eggs/nvim-v1/lua/custom/plugins/diff.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    'esmuellert/vscode-diff.nvim',
+    dependencies = { 'MunifTanjim/nui.nvim' },
+    cmd = 'CodeDiff',
+  },
+  -- {
+  --   'aaronhallaert/advanced-git-search.nvim',
+  --   cmd = { 'AdvancedGitSearch' },
+  --   config = function()
+  --     require('advanced_git_search.snacks').setup {
+  --       -- Insert Config here
+  --     }
+  --   end,
+  --   dependencies = {
+  --     'folke/snacks.nvim',
+  --   },
+  -- },
+}
