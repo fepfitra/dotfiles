c37016107c6c08034a926aa950b85abcb3759c0b
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2026-01-07 14:03:41 +0700

    fix(nvim): enable rustaceanvim and configure blink.cmp for rust completion

--- eggs/nvim-v1/lua/custom/plugins/rustdebug.lua
+++ eggs/nvim-v1/lua/custom/plugins/rustdebug.lua
@@ -2,4 +2,11 @@ return {
   'mrcjkb/rustaceanvim',
   version = '^6', -- Recommended
   lazy = false, -- This plugin is already lazy
+  config = function()
+    vim.g.rustaceanvim = {
+      server = {
+        capabilities = require('blink.cmp').get_lsp_capabilities(),
+      },
+    }
+  end,
 }
